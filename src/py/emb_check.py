3af49bdf61cd2f9f0b69c871dcc84b30bc975244
msandonini <mirco.ms16@gmail.com>
2026-07-14 01:14:37 +0200

    Trying to fix hashing problems

---
+++ src/py/emb_check.py
@@ -0,0 +1,8 @@
+import torch
+
+data = torch.load("data/embeddings/clip.pt")
+
+print("ref_path - type:", type(data["ref_path"]), "content: ", data["ref_path"])
+print("dist_path - type:", type(data["dist_path"]), "content: ", data["dist_path"])
+print("ref_path[0] - type:", type(data["ref_path"][0]), "content: ", data["ref_path"][0])
+print("dist_path[0] - type:", type(data["dist_path"][0]), "content: ", data["dist_path"][0])
