4efca09fd11f9ea9ef67c22fbde8cbf23acc5fbc
msandonini <mirco.ms16@gmail.com>
2026-07-14 15:35:56 +0200

    Updated metric MLP to solve some errors and add evaluation

--- job_run.sh
+++ job_run.sh
@@ -10,16 +10,16 @@
 PROJ_DIR="/work/cvcs2026/pixel_pioneers"
 VENV_DIR="$PROJ_DIR/.venv"
 
-module load python/3.11.11-gcc-11.4.0
+# module load python/3.11.11-gcc-11.4.0
 
-if [ ! -f "$VENV_DIR/bin/activate" ]; then
-    echo "[job] creating venv..."
-    python -m venv "$VENV_DIR"
-fi
+# if [ ! -f "$VENV_DIR/bin/activate" ]; then
+#    echo "[job] creating venv..."
+#    python -m venv "$VENV_DIR"
+# fi
 
 source "$VENV_DIR/bin/activate"
 
-pip install --upgrade pip
-pip install -r "$PROJ_DIR/requirements.txt"
+# pip install --upgrade pip
+# pip install -r "$PROJ_DIR/requirements.txt"
 
 python "$PROJ_DIR/src/py/metrics_main.py"
